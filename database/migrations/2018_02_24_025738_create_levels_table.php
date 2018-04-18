616d36e5597fdbb2f00282ed03f431a1b8183dc0
Apple <apple@Apples-MacBook-Pro-2.local>
2018-04-18 02:14:15 -0500

    Se agregó la migración LEVELS

--- database/migrations/2018_02_24_025738_create_levels_table.php
+++ database/migrations/2018_02_24_025738_create_levels_table.php
@@ -16,7 +16,6 @@ class CreateLevelsTable extends Migration
         Schema::create('levels', function (Blueprint $table) {
             $table->increments('id');
             $table->string('level');
-            $table->string('description')->nullable();
         });
     }
 
