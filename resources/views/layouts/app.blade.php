36056ca5d7a07170e0fa44aa24caa6a847dc9281
Apple <apple@Apples-MacBook-Pro-2.local>
2018-04-18 02:48:09 -0500

    Se oculta botón Observador

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,7 +61,7 @@
                             {{--  <li><a href="{{ route('register') }}">Register</a></li>  --}}
                         @else
                             <li><a href="{{ route('observer.index')}}">Cursos</a></li>
-                            <li><a href="{{ route('observer.index')}}">Observador</a></li>
+                            {{--  <li><a href="{{ route('observer.index')}}">Observador</a></li>  --}}
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
