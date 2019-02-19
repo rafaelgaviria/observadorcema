f6f0b92bf169d288e0d52826ba3394375f1212c3
rafaelgaviria <rafaelgaviria@gmail.com>
2019-02-18 23:14:28 -0500

    Ajuste controlador cambiar  course_id por course  del metodo index

--- app/Http/Controllers/Admin/ObserverController.php
+++ app/Http/Controllers/Admin/ObserverController.php
@@ -65,7 +65,7 @@ class ObserverController extends Controller
 			$estudiantes[$i] = DB::table('users')
 				->where('role_id', '=', 5)->where('state', '=', TRUE)->where('course', '=', $i)->orderBy('name', 'ASC')->get();
 			
-				$total[$i] = Observer::whereBetween('created_at', [$ini_4p, $end_4p])->where("course","=",$i)->count();
+				$total[$i] = Observer::whereBetween('created_at', [$ini_4p, $end_4p])->where("course_id","=",$i)->count();
 			
 			foreach($estudiantes[$i] as $estudiante){
 				$asistencia[$i][] = DB::table('observations')->where('user_id',$estudiante->id)->whereBetween('created_at', [$ini_4p, $end_4p])
