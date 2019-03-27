2591c05e54ccee006cbc81b901ac51fe76bcfe8f
rafaelgaviria <rafaelgaviria@gmail.com>
2019-03-27 08:47:58 -0500

    Ajuste para visualizacion 2do corte para estudiante

--- resources/views/admin/academic/student/academico.blade.php
+++ resources/views/admin/academic/student/academico.blade.php
@@ -137,7 +137,13 @@
                 <i class="check big icon green"></i>
               @endif
             </td>
-            <td></td>
+            <td style="text-align: center; vertical-align:middle">
+              @if ($ob_academic->cp_02 === 1)
+                <i class="x big icon red"></i>
+              @else
+                <i class="check big icon green"></i>
+              @endif
+            </td>
             <td></td>
             <td></td>
             <td></td>
